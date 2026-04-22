48cc430013be28b710f0d8843130fb32a511b3e3
sbeland312 <sbeland@umd.edu>
2026-04-22 11:35:51 -0400

    Link fix take 3?

--- translation.js
+++ translation.js
@@ -152,7 +152,7 @@ function displayData(data) {
 
       <div class="item-header">
         <h2>
-          <a href="${obj.URL}" target="_blank" rel="noopener noreferrer">
+          <a href="https://snaccooperative.org/view/85524551#holding-repository" target="_blank" rel="noopener noreferrer">
             ${obj.SNAC_Holding_Repository || ''}
           </a>
         </h2>
@@ -166,7 +166,7 @@ function displayData(data) {
           <span class="inline-label">
             ${translations[currentLanguage].resourceTitle}:
           </span>
-          <a href="${obj.URL}" target="_blank" rel="noopener noreferrer">
+          <a href="${obj.Resource_URL}" target="_blank" rel="noopener noreferrer">
             ${obj.Resource_Title || ''}
           </a>
         </h3>
